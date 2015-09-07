eac88b532d1bb578785d428dc8efdd14df013fcf
LukeHaines <lukehaines2@gmail.com>
2015-09-07 19:08:00 +0100

    remembered today

--- js/spotify.js
+++ js/spotify.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   console.log('ding dong');
 
-// GLOBAL VARIABLES
-var searchType = $('#search-option');
-var results = $('#results');
+  // GLOBAL VARIABLES
+  var searchInput = $('#search-area')
+  var searchOption = $('#search-option');
+  var results = $('#results');
 
- searchType.on('change', getSearch);
+  searchOption.on('change', getSearch);
+  submitButton.on('click', getSearch);
 
-
-function getSearch(e) {
-      };
-
+  function getSearch(e) {
+    
+  $.get('https://api.spotify.com/v1/search?q=coldplay&type=artist' + searchInput.val + '&type'
+  };
 });
 
 
