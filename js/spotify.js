4c9c2f29c3ca6b0926c831b9c28da70e3d0cf040
LukeHaines <lukehaines2@gmail.com>
2015-09-08 08:09:54 +0100

    bit of CSS

--- js/spotify.js
+++ js/spotify.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
   function getSearch(e) {
 
-  $.get('https://api.spotify.com/v1/search?q=' + searchInput.val() + '&type=' + searchOption.val(), function(output) {
+    $.get('https://api.spotify.com/v1/search?q=' + searchInput.val() + '&type=' + searchOption.val(), function(output) {
       console.log(output)
       results.empty() 
     
@@ -23,21 +23,17 @@ $(document).ready(function() {
         searchOption.get(0).selectedIndex = 0;
         
         // ATTEMPTING THE MP3 URL PLAY
-        var previewUrl = dropdown2.preview_url;
-        if searchOption.val() === 'track' {
-          <audio src="/test/audio.ogg">
-        <p>Your browser does not support the <code>audio</code> element.</p>
-        </audio>
-        previewUrl
-        
-        }
-        console.log(previewUrl);
+        // var previewUrl = dropdown2.preview_url;
+        // if searchOption.val() === 'track' {
+        //   <audio src="https://p.scdn.co/mp3-preview/b9f7b7cd553c4aee3cd5146cfb068539047f5219">previewUrl
+        // <p>Your browser does not support the <code>audio</code> element.</p>
+        // </audio>
+        // }
+        // console.log(previewUrl);
         })
       })
     }
 });
-
-// move global variables
 
 
 
